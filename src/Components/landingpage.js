7485b9d0ebd64611ae049c52e4812b0298d74e63
Nandhakumar Saravana <nsaravan@usc.edu>
2020-12-19 22:31:03 -0800

    Done with landing

--- src/Components/landingpage.js
+++ src/Components/landingpage.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+import logo from '../images/Port_img.png'
 
 class Landingpage extends Component{
     render() {
         return(
-            <div>
-                <h1> Landing Page</h1>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                        <img
+                            src={logo}
+                            alt="avatar"
+                            className="avatar-img"
+                        >
+                        </img>
+
+                        <div className="banner-text">
+                            <h1>Nandhakumar Saravana</h1>
+
+                            <hr>
+                            </hr>
+
+                            <p>Skills: HTML/CSS  |  C/C++  |  JavaScript  |  React  |  Python  |  SQL  |  Snowflake  |  Java</p>
+
+                            <div className="social-links">
+
+                                {/* LinkedIn */}
+                                <a href="https://www.linkedin.com/in/nandhakumar-saravana-853528188/" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-linkedin-square" aria-hidden="true" />
+                                </a>
+
+                                {/* Github */}
+                                <a href="https://github.com/nandhakumar23" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-github-square" aria-hidden="true" />
+                                </a>
+
+
+                            </div>
+
+                        </div>
+
+
+                    </Cell>
+                </Grid>
             </div>
 
         )
